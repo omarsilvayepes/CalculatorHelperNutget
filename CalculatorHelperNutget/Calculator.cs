8c605d4c5c9c134077e1a81ea1d650f4b085ebeb
omarsilvayepes <omar.yepes@hotmail.com>
2024-01-12 17:33:40 -0500

    Add Base project

---
+++ CalculatorHelperNutget/Calculator.cs
@@ -0,0 +1,32 @@
+﻿namespace CalculatorHelperNutget
+{
+    public class Calculator
+    {
+        public static decimal mutiply(decimal x, decimal y)
+        {
+            return x * y;
+        }
+        public static decimal? Divide(decimal x, decimal y)
+        {
+            try
+            {
+                return x / y;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Division of {0} by  zero.",x);
+                return null;
+            }
+
+            
+        }
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+        public static decimal subtract(decimal x, decimal y)
+        {
+            return x -y;
+        }
+    }
+}
